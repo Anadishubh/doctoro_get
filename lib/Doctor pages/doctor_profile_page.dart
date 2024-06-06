e405673b2ab078d8c23d55d04367a065a9d7a33e
amandevsingh <aman.singh@covalenseglobal.com>
2024-06-06 13:25:33 +0530

    Update privacy_policy.dart

--- lib/Doctor pages/doctor_profile_page.dart
+++ lib/Doctor pages/doctor_profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:doctoro_get/constants/color_constants.dart';
 import 'package:flutter/material.dart';
 
 class DoctorProfilePage extends StatelessWidget {
@@ -6,7 +7,190 @@ class DoctorProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Profile'),),
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(right: 208.0),
+          child: Text('My Profile'),
+        ),
+        backgroundColor: AppColors.primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/doc.png',
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.fill,
+                ),
+                const SizedBox(width: 16.0),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Mr. Anuj Maurya',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '+91 - 9873574896',
+                    ),
+                    Text(
+                      'anuj@gmail.com',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 190,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.greenAccent.shade100),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('Age: 30 Yrs',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.greenAccent.shade100),
+                  width: 197,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('Gender: Male',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.greenAccent.shade100),
+              width: double.maxFinite,
+              padding: const EdgeInsets.all(8.0),
+              child: const Text('Country: India',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 194,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.greenAccent.shade100),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text('City: New Delhi',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 194,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.greenAccent.shade100),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text('Pincode: 110057',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Language Known',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Wrap(
+              spacing: 10.0,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.brown.shade200),
+                  width: 120,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('English',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.brown.shade200),
+                  width: 120,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('Hindi',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.brown.shade200),
+                  width: 135,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text(
+                    'Bengali',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Designation',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.orangeAccent.shade100),
+              width: double.maxFinite,
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'Psychologist',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
